e1bc8da3ab8ab047cd1e4bc93b924e9775b2a106
Haeeun Lee <hegfs6327@naver.com>
2022-06-15 00:29:23 +0900

    Time: 151 ms (16.53%), Space: 45.7 MB (29.63%) - LeetHub

--- 51-n-queens/51-n-queens.js
+++ 51-n-queens/51-n-queens.js
@@ -4,40 +4,28 @@
  */
 var solveNQueens = function (n) {
   let answer = [];
-  let board = [];
-
-  //체스보드 생성
-  for (let i = 0; i < n; i++) {
-    board[i] = [];
-    for (let j = 0; j < n; j++) {
-      board[i][j] = ".";
-    }
-  }
+  let board = Array.from(new Array(n), () => new Array(n).fill('.'));
 
   const nQueen = (i, j, board) => {
-    let currentBoard = [...board];
-
     //같은 줄에 Q 가 있다면 지우기
-    for (let m = 0; m < currentBoard[i].length; m++) {
-      currentBoard[i][m] = ".";
+    for (let m = 0; m < board[i].length; m++) {
+      board[i][m] = ".";
     }
 
     //체스 놓을 수 있다면
-    if (isChess(i, j, n, currentBoard)) {
-      currentBoard[i][j] = "Q";
+    if (isChess(i, j, n, board)) {
+      board[i][j] = "Q";
 
       if (i === n - 1) {
-        const nQueenBoard = currentBoard.map((element) => element.join(""));
+        const nQueenBoard = board.map((element) => element.join(""));
         answer.push(nQueenBoard);
       } else {
-        nQueen(i + 1, 0, currentBoard);
+        nQueen(i + 1, 0, board);
       }
     }
 
     if (j !== n - 1) {
-      nQueen(i, j + 1, currentBoard);
-    } else {
-      return;
+      nQueen(i, j + 1, board);
     }
   };
 
