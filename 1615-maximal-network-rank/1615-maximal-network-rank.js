c84e0d94a7e9fd4a68b2710bc820e784f85f0d5f
Haeeun Lee <hegfs6327@naver.com>
2022-07-03 11:32:57 +0900

    Time: 164 ms (35.00%), Space: 51 MB (26.00%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.js
+++ 1615-maximal-network-rank/1615-maximal-network-rank.js
@@ -1,61 +1,29 @@
-/**
- * @param {number} n
- * @param {number[][]} roads
- * @return {number}
- */
 var maximalNetworkRank = function (n, roads) {
-  let count = Array(n).fill(0);
+  const graph = [];
 
-  roads.forEach((element) => {
-    count[element[0]]++;
-    count[element[1]]++;
-  });
-
-  let max = findMaxRoads(count);
-  let second = {};
-
-  if (max.roads.length === 1) {
-    count = count.map((element) => {
-      return element === max.count ? -1 : element;
-    });
-    second = findMaxRoads(count);
-  } else {
-    second = max;
+  for (let i = 0; i < n; i++) {
+    graph.push(Array(n).fill(0));
   }
 
-  let answer = max.count + second.count;
-  return checkNotConnectRoads(roads, max.roads, second.roads)
-    ? answer
-    : answer - 1;
-};
+  for (const [from, to] of roads) {
+    graph[from][to] = 1;
+    graph[to][from] = 1;
+  }
 
-const findMaxRoads = (count) => {
-  let maxRoads = [];
-  let maxCount = Math.max(...count);
+  const ranks = [];
 
-  for (let i = 0; i < count.length; i++) {
-    if (count[i] === maxCount) {
-      maxRoads.push(i);
+  for (let i = 0; i < n; i++) {
+    ranks[i] = graph[i].filter((x) => x > 0).length;
+  }
+
+  let max = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const res = ranks[i] + ranks[j] + (graph[i][j] > 0 ? -1 : 0);
+      max = Math.max(max, res);
     }
   }
-  return { count: maxCount, roads: maxRoads };
-};
 
-const checkNotConnectRoads = (roads, max, second) => {
-  let check = false;
-  roads = roads.map((element) => (element = element[0] + " " + element[1]));
-
-  for (let i = 0; i < max.length; i++) {
-    for (let j = 0; j < second.length; j++) {
-      if (max[i] === second[j]) continue;
-      if (
-        !roads.includes(max[i] + " " + second[j]) &&
-        !roads.includes(second[j] + " " + max[i])
-      ) {
-        check = true;
-      }
-    }
-  }
-    
-  return check;
+  return max;
 };
