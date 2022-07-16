a673c4d8079bf7914fb8c51eecc80cd49769b509
Haeeun Lee <hegfs6327@naver.com>
2022-07-16 23:51:04 +0900

    Time: 317 ms (29.12%), Space: 50.4 MB (12.32%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -1,13 +1,13 @@
 var canJump = function (nums) {
-  let visit = new Array(nums.length).fill("F");
-  visit[0] = "T";
+  let visit = new Array(nums.length).fill(false);
+  visit[0] = true;
 
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j <= i + nums[i]; j++) {
-      if (visit[i] === "T") visit[j] = "T";
+      if (visit[i]) visit[j] = true;
     }
-    if (visit[nums.length - 1] === "T") break;
+    if (visit[nums.length - 1]) break;
   }
-    
-  return visit[nums.length - 1] === "T";
+
+  return visit[nums.length - 1]
 };
