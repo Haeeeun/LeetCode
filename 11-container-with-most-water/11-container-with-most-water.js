6fd991f826bef487cb3b8a7cf16a10f7d2a96650
Haeeun Lee <hegfs6327@naver.com>
2022-07-16 23:10:48 +0900

    Time: 83 ms (89.48%), Space: 49.7 MB (46.61%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,13 @@
+var maxArea = function (height) {
+  let max = 0;
+  let point1 = 0;
+  let point2 = height.length - 1;
+
+  while (point2 > point1) {
+    let area = Math.min(height[point1], height[point2]) * (point2 - point1);
+    if (area > max) max = area;
+    height[point1] > height[point2] ? point2-- : point1++;
+  }
+
+  return max;
+};
